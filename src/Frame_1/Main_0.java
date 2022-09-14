8070469ce53ab7159854ddb238d2e3becdc86e85
Denkyung <san7952@gmail.com>
2022-09-14 11:22:55 +0900

    foundation ended

---
+++ src/Frame_1/Main_0.java
@@ -0,0 +1,11 @@
+package Frame_1;
+
+import java.util.Scanner;
+
+public class Main_0 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+	}
+}
